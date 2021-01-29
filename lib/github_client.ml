030b192fa78ab6b0203ccd151752fce5bf119a21
Etienne Millon <me@emillon.org>
2021-01-29 16:49:44 +0100

    Use curl to download instead of cohttp
    This moves the HTTP and TLS concerns out of this plugin. Using curly
    (which shells out to curl) is reasonably portable and is what
    dune-release does.
    
    Down the line we might want to even use `OpamDownload` since it's
    guaranteed to be available, but the changes in that PR are compatible
    with that approach too.
    
    This also includes a small client for locally testing the github client.
    
    Closes #9

--- lib/github_client.ml
+++ lib/github_client.ml
@@ -17,22 +17,38 @@ module Real = struct
     | Some repo -> Ok repo.repository_name
     | None -> Error `Unknown
 
-  let get_pr { Pull_request.user; repo; number } =
-    let open Github.Monad in
-    Github.Pull.get ~user ~repo ~num:number () >|= Github.Response.value
-
-  let pr_info pr =
-    let open Rresult.R in
+  let info_of_pull pull =
     let open Let_syntax.Result in
-    let* { Github_t.pull_head; pull_title = title; _ } =
-      get_pr pr |> Github.Monad.run |> Lwt_result.catch |> Lwt_main.run
-      |> reword_error (fun (_ : exn) -> `Unknown)
-    in
+    let { Github_t.pull_head; pull_title = title; _ } = pull in
     let* user = pull_source_user pull_head in
     let+ repo = pull_source_repo pull_head in
     let branch = pull_head.branch_ref in
     let source_branch = { Branch.user; repo; branch } in
     { source_branch; title }
+
+  let check_code code = if code = 200 then Ok () else Error `Unknown
+
+  let parse_pull body =
+    match Github_j.pull_of_string body with
+    | pull -> Ok pull
+    | exception Yojson.Json_error _ -> Error `Unknown
+
+  let pull_url pr =
+    let root = "https://api.github.com" in
+    let { Pull_request.user; repo; number; _ } = pr in
+    Printf.sprintf "%s/repos/%s/%s/pulls/%d" root user repo number
+
+  let get url =
+    Curly.get url
+    |> Rresult.R.reword_error (fun (_ : Curly.Error.t) -> `Unknown)
+
+  let pr_info pr =
+    let open Let_syntax.Result in
+    let url = pull_url pr in
+    let* response = get url in
+    let* () = check_code response.code in
+    let* pull = parse_pull response.body in
+    info_of_pull pull
 end
 
 let real =
---
+++ test/client/README.md
@@ -0,0 +1,2 @@
+A small executable to run a github client against the github API for manual
+testing.
