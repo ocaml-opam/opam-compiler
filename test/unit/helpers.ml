a009346df3df0af4e7e4fe63aaa77859afe87a99
Etienne Millon <me@emillon.org>
2021-07-13 17:25:11 +0200

    Extend Mock to support multiple functions
    And test using that

--- test/unit/helpers.ml
+++ test/unit/helpers.ml
@@ -3,8 +3,3 @@ open Opam_compiler
 let github_client_fail_all =
   let pr_info _ = assert false in
   { Github_client.pr_info }
-
-let runner_fail_all =
-  let run ?extra_env:_ _ = assert false in
-  let run_out ?extra_env:_ _ = assert false in
-  { Runner.run; run_out }
--- test/unit/mock.mli
+++ test/unit/mock.mli
@@ -11,3 +11,10 @@ val create :
     A check function is returned that should be run at the end of its lifetime.
     It is possible to use [(let$)] to run it at the end of a binding, e.g.
     [let$ f = create ... in ...]. *)
+
+val create2 :
+  'a1 Alcotest.testable ->
+  'a2 Alcotest.testable ->
+  string ->
+  (('a1, 'b1) expectation, ('a2, 'b2) expectation) Either.t list ->
+  ('a1 -> 'b1) * ('a2 -> 'b2) * (unit -> unit)
