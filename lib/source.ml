00380f60e38c5df5cd2307449f0c66d3ba386b26
Etienne Millon <me@emillon.org>
2021-09-08 16:02:02 +0200

    Support github URL for pull requests

--- lib/source.ml
+++ lib/source.ml
@@ -4,6 +4,28 @@ type t = Github_branch of Branch.t | Github_PR of Pull_request.t
 
 let github_pr pr = Github_PR pr
 
+let parse_as_pr_url s =
+  let re =
+    Re.compile
+      (Re.seq
+         [
+           Re.bos;
+           Re.str "https://github.com/";
+           Re.group Pull_request.user_re;
+           Re.str "/";
+           Re.group Pull_request.repo_re;
+           Re.str "/pull/";
+           Re.group (Re.rep1 Re.digit);
+           Re.eos;
+         ])
+  in
+  let open Let_syntax.Option in
+  let+ g = Re.exec_opt re s in
+  let user = Re.Group.get g 1 in
+  let repo = Re.Group.get g 2 in
+  let number = Re.Group.get g 3 |> int_of_string in
+  Github_PR { user; repo; number }
+
 let parse_as_branch s =
   let branch_name = Re.rep1 Re.any in
   let re_branch =
@@ -28,10 +50,13 @@ let parse_as_branch s =
 let parse_as_pr s = Option.map github_pr (Pull_request.parse s)
 
 let parse s =
-  match parse_as_branch s with
+  match parse_as_pr_url s with
   | Some r -> Ok r
   | None -> (
-      match parse_as_pr s with Some r -> Ok r | None -> Error `Unknown)
+      match parse_as_branch s with
+      | Some r -> Ok r
+      | None -> (
+          match parse_as_pr s with Some r -> Ok r | None -> Error `Unknown))
 
 let pp ppf = function
   | Github_branch branch ->
